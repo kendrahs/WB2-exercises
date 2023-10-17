152830342b73bb1ee82f775e64659b68388ddd0a
kendrah <kentinez11@gmail.com>
2023-10-17 15:46:02 -0400

    Finished tip amount

---
+++ MathScripts/tip_amount.js
@@ -0,0 +1,10 @@
+var foodCost = 30;
+var percent = 7;
+
+var tipAmount = foodCost/percent;
+
+var totalAmount = foodCost/percent + foodCost;
+
+console.log("The tip on a $30 food bill is " + "$" + tipAmount.toFixed(2) + ".")
+
+console.log("So the total is " + "$" + totalAmount.toFixed(2) + ".")
