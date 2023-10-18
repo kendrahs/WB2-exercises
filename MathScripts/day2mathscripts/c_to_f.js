645dc39eea5439aa85615414429ac650b2513516
kendrah <kentinez11@gmail.com>
2023-10-18 09:54:26 -0400

    Finished temp converters

---
+++ MathScripts/day2mathscripts/c_to_f.js
@@ -0,0 +1,6 @@
+var celcius = 7
+
+var celToFarConversion = (celcius * 9/5) + 32
+
+//rounded up
+console.log ("At a temperature of " + celcius + "°C, it's " + Math.round(celToFarConversion) + "°F.")
---
+++ MathScripts/day2mathscripts/f_to_c.js
@@ -0,0 +1,6 @@
+var farenheit = 70
+
+var farToCelConversion = (farenheit - 32) / (9/5)
+
+//rounded up
+console.log ("At a temperature of " + farenheit + "°F, it's " + Math.round(farToCelConversion) + "°C.")
