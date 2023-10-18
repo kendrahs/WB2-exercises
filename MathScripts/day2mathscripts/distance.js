9049b5349fc63691647f3f138aff86ef696dd8f2
kendrah <kentinez11@gmail.com>
2023-10-18 10:57:39 -0400

    Started distance, have to add ^2

---
+++ MathScripts/day2mathscripts/distance.js
@@ -0,0 +1,9 @@
+var x1 = 4
+var x2 = 5
+var y1 = 9
+var y2 = 7
+
+
+var distance = (x2-x1) - (y2-y1)
+
+console.log ("The distance between the two points is " + distance + " units.")
---
+++ MathScripts/day2mathscripts/rentals.js
@@ -0,0 +1,5 @@
+var numberOfPeople = 
+
+var numberOfVans = 
+var vanRentCost = 
+var eachPersonPayment = 
