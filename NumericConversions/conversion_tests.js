4034aefef2724ca58e710edfdf4692888d721afb
kendrah <kentinez11@gmail.com>
2023-10-18 15:49:28 -0400

    Unary attempt

--- NumericConversions/conversion_tests.js
+++ NumericConversions/conversion_tests.js
@@ -1,2 +1,22 @@
 //Description: This script test various numeric conversion techniques
 //Author: Kendrah M. Newprogrammer
+
+var a = " 101.1 ";
+var b = "55";
+var c = "402 Stevens";
+var d = "Number 5 ";
+
+//parseInt () results
+
+
+//parseFloat () results
+
+
+//Number () results
+
+
+//Unary + results
+a = +a;
+b = +b;
+c = +c;
+d = +d;
