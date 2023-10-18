0b44f691218ebf26f6239656f5522d6dab5e4775
kendrah <kentinez11@gmail.com>
2023-10-18 11:10:29 -0400

    Rentals, minor erros

--- MathScripts/day2mathscripts/rentals.js
+++ MathScripts/day2mathscripts/rentals.js
@@ -1,5 +1,7 @@
-var numberOfPeople = 
+var numberOfPeople = 12
 
-var numberOfVans = 
-var vanRentCost = 
-var eachPersonPayment = 
+var numberOfVans = numberOfPeople / 15
+var vanRentCost = 250 * numberOfVans
+var eachPersonPayment = vanRentCost / numberOfPeople
+
+console.log ("We have " + numberOfPeople + " people going on a tour, so we'll be needing " + Math.round(numberOfVans) + " van(s). That's $" + vanRentCost.toFixed(2) + ". If we are to split, the payment evenly per person, it'll come out to be $" + eachPersonPayment.toFixed(2) + ".")
