2aefc46ac3448ffd2609f0568f29477c135c928e
kendrah <kentinez11@gmail.com>
2023-10-18 09:25:56 -0400

    Finished rule of 72

---
+++ MathScripts/rule_of_72.js
@@ -0,0 +1,6 @@
+var insterestRateInPercentageWhole = 10;
+var howManyYearsToDouble = 72/insterestRateInPercentageWhole
+
+var message = "At a " + insterestRateInPercentageWhole + " percent interest rate, savings account will be worth $" + howManyYearsToDouble + "."
+
+console.log(message)
