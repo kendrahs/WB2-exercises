c550b8d25b7ab30524e7517e057ebd07c26e3a73
kendrah <kentinez11@gmail.com>
2023-10-18 10:26:00 -0400

    Finished tile box, but unsure

---
+++ MathScripts/day2mathscripts/tiles.js
@@ -0,0 +1,7 @@
+var roomLength = 20
+var roomWidth = 9
+
+var numberOfTileBoxesForRoom = (roomLength * roomWidth) / 12
+var numberOfExtraBoxes = numberOfTileBoxesForRoom + (10/numberOfTileBoxesForRoom)
+
+console.log ("I'll need " + Math.round(numberOfTileBoxesForRoom) + " boxes to tile the entire room, but I'll buy " + Math.round(numberOfExtraBoxes) + " more in case of messups.")
