cf5a843c2800a762a302a17857d471785a17524a
kendrah <kentinez11@gmail.com>
2023-10-18 10:49:31 -0400

    Fixed tiles

--- MathScripts/day2mathscripts/tiles.js
+++ MathScripts/day2mathscripts/tiles.js
@@ -2,6 +2,6 @@ var roomLength = 20
 var roomWidth = 9
 
 var numberOfTileBoxesForRoom = (roomLength * roomWidth) / 12
-var numberOfExtraBoxes = numberOfTileBoxesForRoom + (10/numberOfTileBoxesForRoom)
+var numberOfExtraBoxes = 10 * numberOfTileBoxesForRoom / 100
 
 console.log ("I'll need " + Math.round(numberOfTileBoxesForRoom) + " boxes to tile the entire room, but I'll buy " + Math.round(numberOfExtraBoxes) + " more in case of messups.")
