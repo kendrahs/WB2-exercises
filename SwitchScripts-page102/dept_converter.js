90c296954724637c075fa451cd3a4fe82b910111
kendrah <kentinez11@gmail.com>
2023-10-19 16:19:37 -0400

    Switch practice

---
+++ SwitchScripts-page102/dept_converter.js
@@ -0,0 +1,32 @@
+//number and name variables
+let depName;
+let depNum = 5;
+
+//switches printing out
+switch (depNum) {
+    case 1:
+    dayName = "Marketing";
+    break;
+    case 5:
+    dayName = "Human Resources";
+    break;
+    case 10:
+    depName = "Accounting";
+    break;
+    case 12:
+    depName = "Legal";
+    break;
+    case 18:
+    dayName = "IT";
+    break;
+    case 20:
+    dayName = "Customer Relations";
+    break;
+    
+    default:
+    // code block for none of the above
+    depName = "<unknown>";
+   }
+   
+
+   console.log( 1)
