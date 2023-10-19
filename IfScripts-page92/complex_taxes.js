850b40e6b7910473ebb728127d420c4938b27faf
kendrah <kentinez11@gmail.com>
2023-10-19 14:23:17 -0400

    Worked more on complex taxes

--- IfScripts-page92/complex_taxes.js
+++ IfScripts-page92/complex_taxes.js
@@ -1,7 +1,7 @@
 "use script";
 //variables
-let payRate = 40;
-let hoursWorked = 50;
+let payRate = 48;
+let hoursWorked = 43;
 let overtimeHours = hoursWorked - 40
 
 let taxRate;
@@ -12,13 +12,30 @@ let annualIncome = grossPay * 52
 
 
 //single filers
-if (annualIncome < 12000) {
+var singleFile = false;
 
+if (singleFile == true) {
+    if (annualIncome < 12000) {
+        taxRate = 5;
+    }
+    else if (annualIncome < 24999) {
+        taxRate = 10;
+    } 
+    else if (annualIncome < 75000) {
+        taxRate = 15;
+    } 
+    else {
+        (annualIncome < 75000)
+        taxRate = 20;
+       
 }
 
 //joint filers
 
 
-
 //calculate tax based on gross
-console.log("You worked " + hoursWorked + " at $" + payRate + " an hour. That leads to a gross pay of $" + grossPay +". Because your filing status is " + + ", your tax withholding this period is $" + + " and you net pay comes out to be $" + + ".") 
+var withholding = grossPay - (taxRate/grossPay)
+
+
+//message
+console.log("You worked " + hoursWorked + " at $" + payRate + " an hour. That leads to a gross pay of $" + grossPay +". Because your filing status is " + singleFile + ", your tax withholding this period is $" + + " and you net pay comes out to be $" + + ".")}
