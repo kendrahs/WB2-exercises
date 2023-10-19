b99b8169d1b0c42b7cbef250f3bbb3dbc373b2c3
kendrah <kentinez11@gmail.com>
2023-10-19 09:03:06 -0400

    Fixed rentals

--- MathScripts/day2mathscripts/rentals.js
+++ MathScripts/day2mathscripts/rentals.js
@@ -1,7 +1,7 @@
-var numberOfPeople = 12
+var numberOfPeople = 85
 
-var numberOfVans = numberOfPeople / 15
+var numberOfVans = numberOfPeople / 15 //divide total number of passengers by the number of passengers each van can accommodate
 var vanRentCost = 250 * numberOfVans
 var eachPersonPayment = vanRentCost / numberOfPeople
 
-console.log ("We have " + numberOfPeople + " people going on a tour, so we'll be needing " + Math.round(numberOfVans) + " van(s). That's $" + vanRentCost.toFixed(2) + ". If we are to split, the payment evenly per person, it'll come out to be $" + eachPersonPayment.toFixed(2) + ".")
+console.log ("We have " + numberOfPeople + " people going on a tour, so we'll be needing " + Math.round(numberOfVans) + " van(s). That's $" + vanRentCost.toFixed(2) + ". If we are to split, the payment evenly per person it'll come out to be $" + eachPersonPayment.toFixed(2) + " per person.")
