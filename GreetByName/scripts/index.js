b56eea74f7c094c54710497fc5480fa46760e8ee
kendrah <kentinez11@gmail.com>
2023-10-20 15:19:21 -0400

    .

---
+++ GreetByName/scripts/index.js
@@ -0,0 +1,19 @@
+"use strict";
+
+window.onload = init 
+
+function init() {
+    const greetUserBtn = document.getElementById;
+    greetUserBtn.onclick = GreetUserBtnClicked;
+}
+
+
+
+function greetUserBtnClicked() {
+    alert('Hello ${}')
+}
+
+function GreetUserBtnClicked() {
+    document.getElementById = greetBtn
+    console.log
+}
